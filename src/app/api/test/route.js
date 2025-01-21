bb485078ab5b4f18d795e92f4d5aa60238d6b780
abhishekneelamsharma <abhisheksharma0573@gmail.com>
2025-01-21 11:43:01 +0530

    msg

--- src/app/api/test/route.js
+++ src/app/api/test/route.js
@@ -10,6 +10,6 @@
 export async function GET(request) {
     return new Response(JSON.stringify({
       NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
-      // NEXTAUTH_SECRET_NEW: process.env.NEXTAUTH_SECRET,
+      NEXTAUTH_URL: process.env.NEXTAUTH_URL,
     }), { status: 200 });
   }
