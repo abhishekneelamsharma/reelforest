01123a9e6e077cbebd30ea536b607f343f35d200
abhishekneelamsharma <abhisheksharma0573@gmail.com>
2025-01-21 11:33:11 +0530

    msg

--- src/app/api/test/route.js
+++ src/app/api/test/route.js
@@ -9,7 +9,7 @@
 
 export async function GET(request) {
     return new Response(JSON.stringify({
-      NEXTAUTH_SECRET: process.env.AUTH_SECRET,
-      NEXTAUTH_SECRET_NEW: process.env.NEXTAUTH_SECRET,
+      NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+      // NEXTAUTH_SECRET_NEW: process.env.NEXTAUTH_SECRET,
     }), { status: 200 });
   }
--- src/middleware.js
+++ src/middleware.js
@@ -3,7 +3,7 @@ import { getToken } from "next-auth/jwt"
 
 export async function middleware(request) {
 
-    const token = await getToken({ req: request, secret: process.env.AUTH_SECRET })
+    const token = await getToken({ req: request, secret: process.env.NEXTAUTH_SECRET })
     const pathname = request.nextUrl.pathname;
 
     const excludedPaths = [
