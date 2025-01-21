e38d4d949a01f8850b14c9a389149b096063f3a3
abhishekneelamsharma <abhisheksharma0573@gmail.com>
2025-01-21 11:06:07 +0530

    env

--- src/app/api/test/route.js
+++ src/app/api/test/route.js
@@ -1,8 +1,15 @@
+// export async function GET(request) {
+//     return new Response(JSON.stringify({ message: "GET request successful!" }), {
+//         status: 200,
+//         headers: {
+//             "Content-Type": "application/json",
+//         },
+//     });
+// }
+
 export async function GET(request) {
-    return new Response(JSON.stringify({ message: "GET request successful!" }), {
-        status: 200,
-        headers: {
-            "Content-Type": "application/json",
-        },
-    });
-}
+    return new Response(JSON.stringify({
+      NEXTAUTH_SECRET: process.env.AUTH_SECRET,
+      NEXTAUTH_SECRET_NEW: process.env.NEXTAUTH_SECRET,
+    }), { status: 200 });
+  }
